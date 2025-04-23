f3f4b51715df932fcfaab1c24bd3c64d5cc83f18
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-04-23 17:28:28 +0300

    Updated some DB tables

--- src/main/resources/db/changelog/changes/002-create-professors-table.sql
+++ src/main/resources/db/changelog/changes/002-create-professors-table.sql
@@ -1,9 +1,9 @@
 create table professors (
                             id            bigint       not null primary key,
-                            first_name    varchar(255) not null,
-                            last_name     varchar(255) not null,
-                            department_id bigint       not null,
-                            type          varchar(50)  not null,
+                            first_name    varchar(255) ,
+                            last_name     varchar(255) ,
+                            department_id bigint       ,
+                            type          varchar(50)  ,
                             user_id       bigint       unique,
                             constraint fk_professor_department foreign key (department_id) references departments (id),
                             constraint fk_user_professor foreign key (user_id) references users (id)
--- src/main/resources/db/changelog/changes/004-create-students-table.sql
+++ src/main/resources/db/changelog/changes/004-create-students-table.sql
@@ -1,10 +1,10 @@
 create table students (
                           id               bigint       not null primary key,
-                          first_name       varchar(255) not null,
-                          last_name        varchar(255) not null,
-                          registration_number varchar(50) not null,
-                          birth_date       date         not null,
-                          group_id         bigint       not null,
+                          first_name       varchar(255) ,
+                          last_name        varchar(255) ,
+                          registration_number varchar(50) ,
+                          birth_date       date         ,
+                          group_id         bigint       ,
                           user_id          bigint       unique,
                           constraint fk_student_group foreign key (group_id) references groups (id),
                           constraint fk_user_student foreign key (user_id) references users (id)
