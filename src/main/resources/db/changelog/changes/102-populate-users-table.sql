6d06bd00886510030038a29cdb3124154c282064
Lungu Mihai <lungu2003mihai1@mail.ru>
2025-05-01 13:43:41 +0300

    Small changes in sql scripts and added the email info for Observer

--- src/main/resources/db/changelog/changes/102-populate-users-table.sql
+++ src/main/resources/db/changelog/changes/102-populate-users-table.sql
@@ -3,5 +3,5 @@ insert into users (id, email, password, role) values
                                                   (nextval('user_id_seq'), 'student2@utm.md', 'password2', 'STUDENT'),
                                                   (nextval('user_id_seq'), 'professor1@utm.md', 'password3', 'PROFESSOR'),
                                                   (nextval('user_id_seq'), 'professor2@utm.md', 'password4', 'PROFESSOR'),
-                                                  (nextval('user_id_seq'), 'assistant@utm.md', 'password5', 'ASSISTANT'),
+                                                  (nextval('user_id_seq'), 'assistant@utm.md', 'password5', 'PROFESSOR'),
                                                   (nextval('user_id_seq'), 'admin@utm.md', 'adminpassword', 'ADMIN');
--- src/main/resources/db/changelog/changes/105-populate-students-table.sql
+++ src/main/resources/db/changelog/changes/105-populate-students-table.sql
@@ -1,5 +1,7 @@
-insert into students (id, first_name, last_name, registration_number, birth_date, group_id, user_id) values
-                                                                                                         (nextval('student_id_seq'), 'George', 'Ionescu', 'S1234', '2000-05-12', 1, 1),
-                                                                                                         (nextval('student_id_seq'), 'Ana', 'Popa', 'S1235', '1999-03-22', 2, 2),
-                                                                                                         (nextval('student_id_seq'), 'Alex', 'Vasiliu', 'S1236', '2001-07-15', 3, 3),
-                                                                                                         (nextval('student_id_seq'), 'Elena', 'Cristea', 'S1237', '1998-11-02', 4, 4);
+insert into students (id, first_name, last_name, registration_number, birth_date, group_id, user_id)
+values (nextval('student_id_seq'), 'George', 'Ionescu', 'S1234', '2000-05-12', 1, 1),
+       (nextval('student_id_seq'), 'Ana', 'Popa', 'S1235', '1999-03-22', 2, 2),
+       (nextval('student_id_seq'), 'Alex', 'Vasiliu', 'S1236', '2001-07-15', 3, 3),
+       (nextval('student_id_seq'), 'Elena', 'Cristea', 'S1237', '1998-11-02', 4, 4),
+       (nextval('student_id_seq'), 'Alex', 'Cristea', 'S1239', '1998-11-02', 1, 5),
+       (nextval('student_id_seq'), 'Elena', 'Costea', 'S12312', '1998-11-02', 1, 6);
